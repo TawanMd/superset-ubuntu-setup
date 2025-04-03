16f19c37831b3767875aaeb6bda7817bb961cfac
TawanMd <thauanmatheus@hotmail.com>
2025-04-03 16:33:19 -0300

    Configuração inicial do Apache Superset (Ubuntu 24.04 / Python 3.10)

---
+++ superset_config.py
@@ -0,0 +1,9 @@
+#---------------------------------------------------------
+# Superset specific config
+#---------------------------------------------------------
+SECRET_KEY = '2IshPLLVn9/zsBcIq3afXUjkwMx/xyNkQ2DmpUga+AJ5szpEatXd7evd'
+
+#---------------------------------------------------------
+# Flask App Builder configuration
+#---------------------------------------------------------
+# Your App builder config here
